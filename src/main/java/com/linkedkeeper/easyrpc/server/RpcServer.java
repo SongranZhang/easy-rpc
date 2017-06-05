b3e337c8311f5ece6c1d01dba099c393f1acdeb8
sauron zhang <sauronzhang@hotmail.com>
2017-06-05 18:19:46 +0800

    modify rpc server logic

--- src/main/java/com/linkedkeeper/easyrpc/server/RpcServer.java
+++ src/main/java/com/linkedkeeper/easyrpc/server/RpcServer.java
@@ -5,10 +5,7 @@ import com.linkedkeeper.easyrpc.codec.RpcEncoder;
 import com.linkedkeeper.easyrpc.codec.RpcRequest;
 import com.linkedkeeper.easyrpc.codec.RpcResponse;
 import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelInitializer;
-import io.netty.channel.ChannelOption;
-import io.netty.channel.EventLoopGroup;
+import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
@@ -17,6 +14,7 @@ import org.apache.commons.collections4.MapUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
@@ -27,14 +25,17 @@ import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
-public class RpcServer implements ApplicationContextAware, InitializingBean {
+public class RpcServer implements ApplicationContextAware, InitializingBean, DisposableBean {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(RpcServer.class);
 
-    private String serverAddress;
-    private Map<String, Object> handleMap = new HashMap<String, Object>();
-
     private static ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(16, 16, 600L, TimeUnit.SECONDS, new ArrayBlockingQueue<Runnable>(65536));
+
+    private String serverAddress;
+    private Map<String, Object> handleMap = new HashMap();
+
+    EventLoopGroup bossGroup = new NioEventLoopGroup();
+    EventLoopGroup workerGroup = new NioEventLoopGroup();
 
     public RpcServer(String serverAddress) {
         this.serverAddress = serverAddress;
@@ -51,35 +52,51 @@ public class RpcServer implements ApplicationContextAware, InitializingBean {
     }
 
     public void afterPropertiesSet() throws Exception {
-        EventLoopGroup bossGroup = new NioEventLoopGroup();
-        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        ServerBootstrap bootstrap = new ServerBootstrap();
+        bootstrap.group(bossGroup, workerGroup)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<SocketChannel>() {
+                    public void initChannel(SocketChannel ch) throws Exception {
+                        ch.pipeline()
+                                .addLast(new LengthFieldBasedFrameDecoder(65536, 0, 4, 0, 0))
+                                .addLast(new RpcDecoder(RpcRequest.class))
+                                .addLast(new RpcEncoder(RpcResponse.class))
+                                .addLast(new RpcHandler(handleMap));
+                    }
+                })
+                .option(ChannelOption.SO_BACKLOG, 128)
+                .childOption(ChannelOption.SO_KEEPALIVE, true);
+
+        String[] array = serverAddress.split(":");
+        String host = array[0];
+        int port = Integer.parseInt(array[1]);
+
+        ChannelFuture future = bootstrap.bind(host, port).sync();
+        ChannelFuture channelFuture = future.addListener(new ChannelFutureListener() {
+            public void operationComplete(ChannelFuture future) throws Exception {
+                if (future.isSuccess()) {
+                    LOGGER.info("Server have success bind to " + serverAddress);
+                } else {
+                    LOGGER.error("Server fail bind to " + serverAddress);
+                    throw new Exception("Server start fail !", future.cause());
+                }
+            }
+        });
+
         try {
-            ServerBootstrap bootstrap = new ServerBootstrap();
-            bootstrap.group(bossGroup, workerGroup)
-                    .channel(NioServerSocketChannel.class)
-                    .childHandler(new ChannelInitializer<SocketChannel>() {
-                        public void initChannel(SocketChannel ch) throws Exception {
-                            ch.pipeline()
-                                    .addLast(new LengthFieldBasedFrameDecoder(65536, 0, 4, 0, 0))
-                                    .addLast(new RpcDecoder(RpcRequest.class))
-                                    .addLast(new RpcEncoder(RpcResponse.class))
-                                    .addLast(new RpcHandler(handleMap));
-                        }
-                    })
-                    .option(ChannelOption.SO_BACKLOG, 128)
-                    .childOption(ChannelOption.SO_KEEPALIVE, true);
+            channelFuture.await(5000, TimeUnit.MILLISECONDS);
+            if (channelFuture.isSuccess()) {
+                LOGGER.info("start easy rpc server success.");
+            }
 
-            String[] array = serverAddress.split(":");
-            String host = array[0];
-            int port = Integer.parseInt(array[1]);
-
-            ChannelFuture future = bootstrap.bind(host, port).sync();
-            LOGGER.info("Server started on port {}", port);
-            future.channel().closeFuture().sync();
-        } finally {
-            workerGroup.shutdownGracefully();
-            bossGroup.shutdownGracefully();
+        } catch (InterruptedException e) {
+            LOGGER.error("start easy rpc occur InterruptedException!", e);
         }
+    }
+
+    public void destroy() throws Exception {
+        workerGroup.shutdownGracefully();
+        bossGroup.shutdownGracefully();
     }
 
     public static void submit(Runnable task) {
